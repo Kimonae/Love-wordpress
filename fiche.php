b5c0c9d36bd76ee35bae8ea69365dd1a2facada1
Kimonae <70099141+Kimonae@users.noreply.github.com>
2021-07-12 09:51:37 +0200

    Update fiche.php

--- fiche.php
+++ fiche.php
@@ -3,9 +3,11 @@ $res ='<link rel="stylesheet" href="style.css"/>';
 require_once("connexion.php");
 
 
-if(isset($_GET["id"]) {
-$sql ="SELECT id, Nom, Prenom, Descrip FROM stagiaires WHERE id = id";
+if(isset($_GET["id"])) {
+$id = strip_tags($_GET["id"]);
+$sql ="SELECT id, Nom, Prenom, Descrip FROM stagiaires WHERE id = :id";
 $query = $db->prepare($sql);
+$query->bindValue(":id", $id, PDO::PARAM_INT); 
 $query->execute();
 
 
@@ -14,14 +16,15 @@ while( $d = $query->fetch()){
 
     $res.= "<div class='apprenants'>";
     $res.= " ".$d['Prenom'] ." ". $d['Nom'] ."<br>";
-    $res.= .$d['Descrip'];
+    $res.= $d['Descrip'];
     //$res.= .$d['Image'];
     
     $res.= "</div>";
-
+    
 }
 }
 
 
-?>
+
+
 ?>
