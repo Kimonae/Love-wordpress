de67a1f482ed85ac0e8dbd8d6bc443ff85befc11
Kimonae <70099141+Kimonae@users.noreply.github.com>
2021-07-13 10:22:14 +0200

    Update fiche.php

--- fiche.php
+++ fiche.php
@@ -1,30 +1,25 @@
 <?php
+
 $res ='<link rel="stylesheet" href="style.css"/>';
 require_once("connexion.php");
 
-
-if(isset($_GET["id"])) {
-$id = strip_tags($_GET["id"]);
-$sql ="SELECT id, Nom, Prenom, Descrip FROM stagiaires WHERE id = :id";
+$sql ="SELECT id, Nom, Prenom, Descrip, Propos, Imgs FROM stagiaires";
 $query = $db->prepare($sql);
-$query->bindValue(":id", $id, PDO::PARAM_INT); 
 $query->execute();
 
 
 while( $d = $query->fetch()){
-
-
-    $res.= "<div class='apprenants'>";
-    $res.= " ".$d['Prenom'] ." ". $d['Nom'] ."<br>";
-    $res.= $d['Descrip'];
-    //$res.= .$d['Image'];
     
-    $res.= "</div>";
-    
+
+    $res .= "<div class='Promo'>";
+    $res .="<table>";
+    $res .= "<th> ".$d['Prenom'] ."<td> ". $d['Nom'] . " </td> </th>" ."<br>";
+    $res .= "<tr> <th> <td>". $d['Propos'] ." </td> </th> </tr>  <tr> <tr> <th> <td> <br>" ."<img src='../wp-content/themes/72/Imgs/".$d['Imgs'] . "'> </img>" ." </td> </th> </tr> </tr> </tr> <br>";
+    $res.= "<tr>  <th> <td>".'<a href="../wp-content/themes/72/fiche-du-stagiaire/?id='.$d['id'] .'"> <button> Voir + </button> </a>  </td> </th> </tr>';
+    $res .= "</table>";
+    $res .= "</div>";
+
+
 }
-}
-
-
-
 
 ?>
