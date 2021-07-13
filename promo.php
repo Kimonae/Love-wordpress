0dc309122a7ab499341d266a1d30a226d7cc2015
Kimonae <70099141+Kimonae@users.noreply.github.com>
2021-07-13 10:21:44 +0200

    Update promo.php

--- promo.php
+++ promo.php
@@ -15,29 +15,11 @@ while( $d = $query->fetch()){
     $res .="<table>";
     $res .= "<th> ".$d['Prenom'] ."<td> ". $d['Nom'] . " </td> </th>" ."<br>";
     $res .= "<tr> <th> <td>". $d['Propos'] ." </td> </th> </tr>  <tr> <tr> <th> <td> <br>" ."<img src='../wp-content/themes/72/Imgs/".$d['Imgs'] . "'> </img>" ." </td> </th> </tr> </tr> </tr> <br>";
-    $res .= "<tr> <th> <td> voir plus bouton </td> </th> </tr>";
+    $res.= "<tr>  <th> <td>".'<a href="../wp-content/themes/72/fiche-du-stagiaire/?id='.$d['id'] .'"> <button> Voir + </button> </a>  </td> </th> </tr>';
     $res .= "</table>";
     $res .= "</div>";
 
-    /*
-    rajouter
-    La page détail d'un apprenant :
-Une photo du stagiaire
-Son texte 'A propos de moi'
-Ses trois meilleurs projets
-Capture du projet
-Une descriptionWordpress 4
-Les technologies utilisées
-Lien du projet déposé sur le github
-Lien sur hébergement
-Lien vers son github
-Lien vers son linkedin
 
-
-
-
-
-    */
 }
 
 ?>
